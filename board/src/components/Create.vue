88eb55e91667fb95c4d6780d554561305386fe7b
Jungea <jungea2241@gmail.com>
2020-04-19 23:32:34 +0900

    게시판 create, read 기능

---
+++ board/src/components/Create.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>
+        <input type="text" v-model="writer" placeholder="글쓴이" />
+        <input type="text" v-model="title" placeholder="제목" />
+        <textarea v-model="content" placeholder="내용"></textarea>
+        <button @click="write">작성</button>
+    </div>
+</template>
+<script>
+import data from '@/data'
+export default {
+    name: 'Create',
+    data() {
+        return {
+            data: data,
+            writer: '',
+            title: '',
+            content: '',
+        }
+    },
+    methods: {
+        write() {
+            this.data.push({
+                writer: this.writer,
+                title: this.title,
+                content: this.content
+            })
+            this.$router.push({
+                path: '/'
+            })
+        }
+    }
+}
+</script>
--- board/src/router/index.js
+++ board/src/router/index.js
@@ -1,23 +1,22 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Read from '@/components/Read'
+import Create from '@/components/Create'
 
 Vue.use(VueRouter)
 
   const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'Read',
+    component: Read
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/create',
+    name: 'Create',
+    component: Create
+  },
+ 
 ]
 
 const router = new VueRouter({
