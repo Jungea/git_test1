43ed50297eea8b402a8a4d736a3e753a19e2cc65
Jungea <jungea2241@gmail.com>
2020-04-20 21:01:07 +0900

    sidebar 레이아웃 추가

--- board/src/App.vue
+++ board/src/App.vue
@@ -1,34 +1,30 @@
 <template>
-  <div id="app">
-    <router-view/>
-  </div>
+  <b-container id="app" fluid>
+    <b-row class="h-100">
+      <b-col class="myNav h-100 overflow-auto" cols="2"><Header/></b-col>
+      <b-col clos="10"><router-view/></b-col>
+    </b-row>
+  </b-container>
 </template>
 
 <script>
+import Header from '@/components/Header'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    Header,
+  }
 }
 </script>
 
 <style>
+html, body, #app {
+  height: 100%
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+  border: 1px solid red;
 }
-
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
+  .myNav{
+    border: 1px solid gray;
+  }
 </style>
---
+++ board/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+<div>
+  <b-nav vertical class="w-25">
+    <b-nav-item active>Active</b-nav-item>
+    <b-nav-item>Link</b-nav-item>
+    <b-nav-item>Another Link</b-nav-item>
+    <b-nav-item disabled>Disabled</b-nav-item>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam tenetur itaque sequi repellat alias? Eaque id corrupti corporis commodi error provident doloremque nihil laboriosam quod beatae placeat molestiae, quam ut?
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Tempora incidunt dolore ullam deserunt, repellendus corrupti, molestias ad necessitatibus quas, veritatis magni maxime similique! Libero recusandae rerum similique perspiciatis voluptate nisi?
+  </b-nav>
+</div>
+</template>
+<script>
+export default {
+    name: "Header",
+
+}
+</script>
